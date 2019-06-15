f3088ad1a90756e6f2d16d3c1f717c61e3468882
charles161 <fortloop.charles@gmail.com>
2019-06-15 19:07:35 +0530

    users - create, edit, get, and delte methods complete

--- RESTful_API/lib/handlers.js
+++ RESTful_API/lib/handlers.js
@@ -79,11 +79,6 @@ var handler = {
                                     "Hashing error"
                             });
                         }
-                    } else {
-                        callback(500, {
-                            Error:
-                                "The user with that phone number aready exists"
-                        });
                     }
                 });
             } else {
@@ -97,7 +92,7 @@ var handler = {
         get: function (data, callback) {
             //check phone number is valid
             console.log(data.queryObject)
-            phone = typeof (data.queryObject.phone) == "string" && data.queryObject.phone.trim().length == 10 ? data.queryObject.phone : false;
+            let phone = typeof (data.queryObject.phone) == "string" && data.queryObject.phone.trim().length == 10 ? data.queryObject.phone : false;
             if (phone) {
                 _data.read("users", phone, (err, data) => {
                     if (!err && data) {
@@ -113,8 +108,95 @@ var handler = {
                 callback(400, { "Error": "missing required field" })
             }
         },
-        put: function (data, callback) { },
-        delete: function (data, callback) { }
+
+        //TODO: only let an authenticated user to upadte their own object 
+        //required: phone
+        //optional : firstName, lastName and password (atleast one must be specified)
+        put: function (data, callback) {
+            var firstName =
+                typeof data.payload.firstName == "string" &&
+                    data.payload.firstName.trim().length > 0
+                    ? data.payload.firstName.trim()
+                    : false;
+            var lastName =
+                typeof data.payload.lastName == "string" &&
+                    data.payload.lastName.trim().length > 0
+                    ? data.payload.lastName.trim()
+                    : false;
+            var phone =
+                typeof data.payload.phone == "string" &&
+                    data.payload.phone.trim().length == 10
+                    ? data.payload.phone.trim()
+                    : false;
+            var password =
+                typeof data.payload.password == "string" &&
+                    data.payload.password.trim().length > 0
+                    ? data.payload.password.trim()
+                    : false;
+
+            if (phone) {
+                if (firstName || lastName || password) {
+                    _data.read("users", phone, (err, data) => {
+                        if (!err && data) {
+                            if (firstName) {
+                                data.firstName = firstName
+                            }
+                            if (lastName) {
+                                data.lastName = lastName
+                            }
+                            if (password) {
+                                data.hashedpassword = helpers.hash(password)
+                            }
+                            _data.update('users', phone, data, (err) => {
+                                if (err) {
+                                    callback(500, { Error: 'Could not update the user' })
+                                }
+                                else {
+                                    callback(200)
+                                }
+                            })
+                        }
+                        else {
+                            callback(400, { Error: 'The user does not exist' })
+                        }
+                    })
+                }
+                else {
+                    callback(400, { Error: 'missing required field' })
+                }
+            }
+            else {
+                callback(400, { Error: 'missing required field' })
+            }
+        },
+        //users - delete
+        //required field: phone
+        // TODO: only let authenticated users allowed
+        // TODO: cleanup data related to the useer
+        delete: function (data, callback) {
+            var phone =
+                typeof (data.queryObject.phone) == "string" && data.queryObject.phone.trim().length == 10 ? data.queryObject.phone : false;
+
+            if (phone) {
+                _data.read("users", phone, (err, data) => {
+                    if (!err && data) {
+                        _data.delete('users', phone, err => {
+                            if (!err) {
+                                callback(200, { "Message": "User deleted successfully" })
+                            } else {
+                                callback(400, { Error: 'Could not delete the specified user' })
+                            }
+                        })
+                    }
+                    else {
+                        callback(400, { Error: 'The user does not exist' })
+                    }
+                })
+            }
+            else {
+                callback(400, { Error: 'missing required field' })
+            }
+        }
     }
 };
 
